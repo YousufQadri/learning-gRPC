ba55f8b756fcf174652156411f4da8c80255513a
YousufQadri <s.yousuf@live.com>
2019-12-15 22:12:42 +0500

    Modify: Use proto loader to manipulate mongoos e data

--- client.js
+++ client.js
@@ -1,8 +1,10 @@
 const grpc = require("grpc");
-const PROTO_PATH = "./notes.proto";
-const NoteService = grpc.load(PROTO_PATH).NoteService;
-const client = new NoteService(
-  "localhost:50051",
-  grpc.credentials.createInsecure()
-);
+const loader = require("@grpc/proto-loader");
+const PATH = "127.0.0.1:50050";
+
+const packageDefinition = loader.loadSync("./notes.proto");
+const package = grpc.loadPackageDefinition(packageDefinition);
+const Client = package.NoteService;
+const client = new Client(PATH, grpc.credentials.createInsecure());
+
 module.exports = client;
